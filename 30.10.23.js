d640cdc22c203e3eb23710ad8d915042be3bb2b7
Gianmarco Iannella <147939682+G14nm4rc01ann3@users.noreply.github.com>
2023-10-30 18:28:01 +0100

    30.10.23.js

---
+++ 30.10.23.js
@@ -0,0 +1,25 @@
+
+
+let myName = 'Federico';
+
+
+console.log(12 + 20);
+
+
+
+let x = 12;
+
+
+myName = 'Lorenzo';
+const PI = 3.14;
+
+
+let subtraction = 4 - x;
+console.log(subtraction);
+
+
+
+let name1 = 'john';
+let name2 = 'John';
+console.log('name1 e name2 sono uguali:', name1 === name2);
+console.log('name1 e name2 in lowercase sono uguali:', name1.toLowerCase() === name2.toLowerCase());
